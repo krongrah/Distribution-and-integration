81e4b7772e2b4a2122d3bafc2e49e9b143755830
Krongrah <Krongrah@gmail.com>
2019-10-26 16:32:49 +0200

    Server and client

--- Serverside/Server.js
+++ Serverside/Server.js
@@ -1,19 +1,26 @@
-/* eslint-env es6, node */
-/* eslint no-console: 0 */
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-const {Server} = require('ws');
-let wsserver = new Server({ port: 8080, path: '/' });
-let latest = //TODO: figure out whether userid needs handling here
-    { time: Date.now(), msg: "no messages yet", id: "id" };
-
- wsserver.on('connection', ws => {
-    console.log("New client connected");
-    ws.send(JSON.stringify(latest)+"\n");
-    ws.on('close', (code, msg) => console.log("Connection closing", code, msg));
-    ws.on('message', msg => {
-        //TODO: figure out whether userid needs handling here
-        latest = { time: Date.now(), msg: msg, id: "test"};
-        console.log("Message arrived", msg);
-        wsserver.clients.forEach(c => c.send(JSON.stringify(latest)));
-    });
+//calls function when a new connection is established
+app.get('/', function(req, res){
+  //sends the index.html file to the client
+  res.sendFile(__dirname + '/index.html');
 });
+
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
+
+
+
+
+
+
+
