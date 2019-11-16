5a1f51541d330911aa7c4bf4d9bbd45cc1f6e866
Ahmad Hamid <AhmadRHamid@gmail.com>
2019-11-16 17:21:52 +0100

    Update Server.js

--- Serverside/Server.js
+++ Serverside/Server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = 80;
+const fs = require('fs');
+
+// Certificate
+const privateKey = fs.readFileSync(/etc/letsencrypt/live/ahmadhamid/privkey.pem);
+const certificate = fs.readFileSync(/etc/letsencrypt/live/ahmadhamid/cert.pem);
+const ca = fs.readFileSync(/etc/letsencrypt/live/ahmadhamid/chain.pem);
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+}
+
+// Webservers
+const http = require('http').createServer(app);
+const httpPort = 80;
+
+const https = require('https').createServer(credentials, app);
+const httpsPort = 443:
+
+const io = require('socket.io').listen(http);
+
+app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
 //calls function when a new connection is established
 app.get('/', function(req, res){
@@ -24,13 +43,13 @@ function onConnection(socket){
   socket.on('clear', () => socket.broadcast.emit('clear'));
 }
 
-
-
-
 io.on('connection', onConnection);
 
-http.listen(port, () => console.log('listening on port ' + port));
+// Start listening on port 80 and 443
+http.listen(httpPort, () => {
+  console.log('listening on port ' + httpPort);
+});
 
-
-
-
+https.listen(httpsPort, () => {
+  console.log('listening on port ' + httpsPort);
+});
